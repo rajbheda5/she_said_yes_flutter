46331b0bfb79b7ea0350fb41696eb391c222e94c
Jorge <jorge.coca@bmwna.com>
2019-11-09 18:33:54 -0600

    format

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:she_said_yes_flutter/authentication/authentication.dart';
 void main() => runApp(SheSaidYesApp());
 
 class SheSaidYesApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
